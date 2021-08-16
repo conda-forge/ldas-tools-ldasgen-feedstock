4f57dbb341000064080ba7416a0e7fcadf75857c
Duncan Macleod <duncan.macleod@ligo.org>
2021-08-16 09:55:49 +0100

    update recipe
    - clean up requirements
    - add explicit link against librt for Linux

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,17 +3,23 @@
 mkdir -p _build
 pushd _build
 
+# link librt to get clock_gettime on older glibc versions
+if [ "$(uname)" == "Linux" ]; then
+	export LDFLAGS="-lrt ${LDFLAGS}"
+fi
+
 # configure
-cmake ${SRC_DIR} \
-	-DCMAKE_INSTALL_PREFIX=${PREFIX} \
+cmake \
+	${SRC_DIR} \
+	${CMAKE_ARGS} \
 	-DCMAKE_BUILD_TYPE=RelWithDebInfo \
-	-DCMAKE_INSTALL_LIBDIR="lib" \
 	-DCMAKE_DISABLE_FIND_PACKAGE_Doxygen=true
 
 # build
-cmake --build . -- -j${CPU_COUNT}
+cmake --build . --parallel ${CPU_COUNT} --verbose
 
 # test
-ctest -V
+ctest --parallel ${CPU_COUNT} --verbose
 
-cmake --build . --target install
+# install
+cmake --build . --parallel ${CPU_COUNT} --verbose --target install
