bde4ab652373edcba25cd0b5cd7681544048c88c
Duncan Macleod <macleoddm@cardiff.ac.uk>
2026-04-16 11:33:22 +0100

    build: simplify ctest

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -ex
+
 mkdir -p _build
 pushd _build
 
@@ -16,14 +18,7 @@ cmake --build . --parallel ${CPU_COUNT} --verbose
 
 # test
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
-  ctest --verbose || {
-  if [ "$(uname)" == "Linux" ]; then
-    # see https://git.ligo.org/ldastools/LDAS_Tools/-/issues/124
-    echo "WARNING: ctest failed";
-  else
-    exit 1;
-  fi;
-  }
+  ctest --verbose
 fi
 
 # install
